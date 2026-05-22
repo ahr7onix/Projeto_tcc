3af9e749c88d0bb2c5aa832a2ec239002f719f1e
Isac <isacbuzelli3@gmail.com>
2026-05-22 20:08:32 -0300

    Salvo automaticamente em 22/05/2026 20:08:32

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { PacientesModule } from './modules/pacientes/pacientes.module';
+import { PerfilModule } from './modules/perfil/perfil.module';
+import { RegistrosModule } from './modules/registros/registros.module';
+import { SaudeModule } from './modules/saude/saude.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AuthModule,
+    PacientesModule,
+    RegistrosModule,
+    PerfilModule,
+    SaudeModule,
   ],
 })
 export class AppModule {}
