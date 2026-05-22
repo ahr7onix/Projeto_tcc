ad8d1006a2645fc81a4b3ae085c08ac6b9018db4
Isac <isacbuzelli3@gmail.com>
2026-05-22 18:44:00 -0300

    Salvo automaticamente em 22/05/2026 18:44:00

--- web/web_nutricionista/src/pages/app/HomePage.tsx
+++ web/web_nutricionista/src/pages/app/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import { Card, StatTile, EmptyState, PageHeader, Btn } from '../../components/ui'
