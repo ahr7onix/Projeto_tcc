b5e3b3e280b37cd8620bcaba282b977af63c0f8e
Isac <isacbuzelli3@gmail.com>
2026-05-22 20:55:46 -0300

    Salvo automaticamente em 22/05/2026 20:55:46

--- apps/api/src/modules/perfil/perfil.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PerfilController } from './perfil.controller';
-import { PerfilService } from './perfil.service';
-
-@Module({
-  controllers: [PerfilController],
-  providers: [PerfilService],
-})
-export class PerfilModule {}
