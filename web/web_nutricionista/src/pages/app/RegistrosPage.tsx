65c0cec56565ce2ad9de4c88fcbc3886aa7b7267
Isac <isacbuzelli3@gmail.com>
2026-05-22 18:45:42 -0300

    Salvo automaticamente em 22/05/2026 18:45:42

--- web/web_nutricionista/src/pages/app/RegistrosPage.tsx
+++ web/web_nutricionista/src/pages/app/RegistrosPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { Card, PageHeader, ChipGroup, EmptyState, Btn, Input, Textarea, Select, AlertBanner } from '../../components/ui'
 import { api, extractError } from '../../lib/api'
 
